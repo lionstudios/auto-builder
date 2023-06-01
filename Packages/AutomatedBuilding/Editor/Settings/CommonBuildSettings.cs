57a79fd0fe30beb12d973281bd6d229ae548668c
Ozan Celik <ozan.celik@lionstudios.cc>
2023-06-01 14:13:35 +0200

    Moved adapter stabiliser settings to jenkins parameters;

--- Packages/AutomatedBuilding/Editor/Settings/CommonBuildSettings.cs
+++ Packages/AutomatedBuilding/Editor/Settings/CommonBuildSettings.cs
@@ -2,23 +2,10 @@
 
 namespace LionStudios.Editor.AutoBuilder
 {
-    public enum AdAdapterSettings
-    {
-        FailIfNotStableVersion,
-        Ignore,
-    }
-    public enum CsvFetchFailOptions
-    {
-        FailBuild,
-        Continue,
-    }
     public class CommonBuildSettings : ScriptableObject
     {
         public string[] DevAdditionalDefSymbols = {"DEV"};
         public string ScriptsFolder = "Assets/Scripts/";
-
-        public AdAdapterSettings AdAdapterSettings;
-        public CsvFetchFailOptions CsvFetchFailOptions;
 
     }
 }
