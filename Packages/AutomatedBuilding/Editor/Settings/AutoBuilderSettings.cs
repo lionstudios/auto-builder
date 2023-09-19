51c437cc0b1ef45e4a8d0f3a2171b5c66634e84d
BhaumikLion <bhaumik.shukla@lionstudios.cc>
2023-09-19 17:36:47 +0200

    [BLD] Created Export.plist option button added

--- Packages/AutomatedBuilding/Editor/Settings/AutoBuilderSettings.cs
+++ Packages/AutomatedBuilding/Editor/Settings/AutoBuilderSettings.cs
@@ -1,16 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using LionStudios.Suite.Core;
 using UnityEngine;
-using LionStudios.Suite.Core;
-using UnityEngine.Scripting;
 
 namespace LionStudios.Editor.AutoBuilder
 {
     public class AutoBuilderSettings : ILionSettingsInfo
     {
+        [Space(10)]
         public CommonBuildSettings commonBuildSettings;
+        [Space(10)]
         public AndroidBuildSettings androidBuildSettings;
         public IOSBuildSettings iosBuildSettings;
+        [Space(10)]
         public FakeCMDArgsProvider localBuildSettings;
     }
 }
---
+++ Packages/AutomatedBuilding/Editor/Settings/IOSBuilderSettingsDrawer.cs
@@ -0,0 +1,47 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+namespace LionStudios.Editor.AutoBuilder
+{
+    [CustomPropertyDrawer(typeof(IOSBuildSettings))]
+    public class IOSBuilderSettingsDrawer : PropertyDrawer
+    {
+        bool showUI = false;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            showUI = EditorGUILayout.Foldout(showUI, "iOS Build Settings", true);
+            if (showUI)
+            {
+                EditorGUI.indentLevel++;
+                SerializedProperty iterator = property.Copy();
+                bool enterChildren = true;
+
+                while (iterator.NextVisible(enterChildren))
+                {
+                    if (iterator.propertyPath.Contains("ios"))
+                    {
+                        enterChildren = false;
+                        EditorGUILayout.PropertyField(iterator);
+                    }
+                }
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.FlexibleSpace();
+                    if (GUILayout.Button("Create  Export.plist file", GUILayout.Width(250), GUILayout.Height(20)))
+                    {
+                        IOSBuilder.CreateExportPlist();
+                    }
+
+                    GUILayout.FlexibleSpace();
+                }
+                GUILayout.EndHorizontal();
+
+
+                EditorGUI.indentLevel--;
+            }
+            // EditorGUI.EndProperty();
+        }
+    }
+}
