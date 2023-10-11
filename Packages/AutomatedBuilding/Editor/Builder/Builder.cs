7bc6ea55b1c793386611503d40ea6a2c5631a46c
Bhaumik Shukla <104812926+BhaumikLion@users.noreply.github.com>
2023-10-11 17:44:20 +0200

    Initialize settings service before getting it (#22)

--- Packages/AutomatedBuilding/Editor/Builder/Builder.cs
+++ Packages/AutomatedBuilding/Editor/Builder/Builder.cs
@@ -12,26 +12,27 @@ using UnityEngine;
 
 namespace LionStudios.Editor.AutoBuilder
 {
-
     public abstract class Builder
     {
         protected const string SETTINGS_PATH = "Assets/LionStudios/AutomatedBuilding/Editor";
         protected const string DATE_FORMAT = "yy-MM-dd-HH-mm";
-        
+
         public static readonly string COMMON_SETTINGS_PATH = $"{SETTINGS_PATH}/CommonBuildSettings.asset";
-        public static readonly string FAKE_CMD_ARGS_PATH = $"{SETTINGS_PATH}/FakeCMDArgs.asset";//
+        public static readonly string FAKE_CMD_ARGS_PATH = $"{SETTINGS_PATH}/FakeCMDArgs.asset"; //
 
         private readonly ICMDArgsProvider _cmdArgsProvider;
         private readonly bool _isTestEditorBuild;
 
         protected static CommonBuildSettings _commonBuildSettings;
+
         protected static CommonBuildSettings CommonBuildSettings
         {
             get
             {
                 if (_commonBuildSettings == null)
-                    //_commonBuildSettings = AssetDatabase.LoadAssetAtPath<CommonBuildSettings>(COMMON_SETTINGS_PATH);
+                {
                     _commonBuildSettings = LionSettingsService.GetSettings<AutoBuilderSettings>().common;
+                }
 
                 return _commonBuildSettings;
             }
@@ -41,7 +42,7 @@ namespace LionStudios.Editor.AutoBuilder
         protected abstract BuildTargetGroup BuildTargetGroup { get; }
         protected abstract ScriptingImplementation ScriptingImplementation { get; }
 
-    protected abstract BuildPlayerOptions InitializeSpecific(IDictionary<string, string> cmdParamsMap, bool isProduction);
+        protected abstract BuildPlayerOptions InitializeSpecific(IDictionary<string, string> cmdParamsMap, bool isProduction);
 
         protected abstract void CreateBuildDirectory(string path);
 
@@ -58,7 +59,7 @@ namespace LionStudios.Editor.AutoBuilder
 
         void FailBuild(string message)
         {
-            if(Application.isBatchMode)
+            if (Application.isBatchMode)
             {
                 Debug.LogError("\n\n" + message + "\n\n");
                 EditorApplication.Exit(-1);
@@ -72,7 +73,7 @@ namespace LionStudios.Editor.AutoBuilder
         async Task HandleAdAdapters()
         {
             bool result = await LionMaxAdapterStabiliser.InitAdNetworkData();
-            if(!result)
+            if (!result)
             {
                 string message = "Failed to fetch stable ad adapters, possible connection error\nFailing the build.";
                 FailBuild(message);
@@ -88,6 +89,7 @@ namespace LionStudios.Editor.AutoBuilder
                 {
                     mismatchedAdapterNames = LionMaxAdapterStabiliser.GetMismatchAndroidAdapterNames();
                 }
+
                 if (mismatchedAdapterNames.Count != 0)
                 {
                     string message = "Some adapters are out of date:";
@@ -95,10 +97,12 @@ namespace LionStudios.Editor.AutoBuilder
                     {
                         message += "\n - " + s;
                     }
+
                     message += "\nFailing the build.";
                     FailBuild(message);
                 }
             }
+
             Debug.Log("Ad adapter check complete, ad adapters are stable.");
         }
 
@@ -107,17 +111,17 @@ namespace LionStudios.Editor.AutoBuilder
             try
             {
                 AssetDatabase.StartAssetEditing();
-                
+                LionSettingsService.InitializeService();
                 var buildPlayerOptions = Initialize(BuildTargetGroup, CommonBuildSettings.DevAdditionalDefSymbols,
                     ScriptingImplementation, out var isProduction, out var validateAdapters);
 
-                if(validateAdapters)
+                if (validateAdapters)
                 {
                     await HandleAdAdapters();
                 }
 
                 var report = BuildPipeline.BuildPlayer(buildPlayerOptions);
-                
+
                 if (report.summary.result == BuildResult.Succeeded)
                 {
                     OpenFolder(BuildLocation);
@@ -126,7 +130,7 @@ namespace LionStudios.Editor.AutoBuilder
                 {
                     Debug.LogError($"Build Error -> {report.summary.result}");
                 }
-                
+
                 if (_isTestEditorBuild)
                 {
                     OpenFolder(buildPlayerOptions.locationPathName);
@@ -149,7 +153,7 @@ namespace LionStudios.Editor.AutoBuilder
             return res;
         }
 
-        private BuildPlayerOptions Initialize(BuildTargetGroup buildTargetGroup, string[] devAdditionalDefSymbols, 
+        private BuildPlayerOptions Initialize(BuildTargetGroup buildTargetGroup, string[] devAdditionalDefSymbols,
             ScriptingImplementation scriptingImplementation, out bool isProduction, out bool validateAdapters)
         {
             var cmdArgs = _cmdArgsProvider.Args;
@@ -172,13 +176,13 @@ namespace LionStudios.Editor.AutoBuilder
 
             Debug.unityLogger.filterLogType = isProduction ? LogType.Error : LogType.Log;
 
-            if (devAdditionalDefSymbols != null && devAdditionalDefSymbols.Length > 0) 
+            if (devAdditionalDefSymbols != null && devAdditionalDefSymbols.Length > 0)
             {
                 var currentDefineSymbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
                 currentDefineSymbols = GetDefineSymbolsStr(devAdditionalDefSymbols, currentDefineSymbols, isProduction);
                 PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, currentDefineSymbols);
             }
-            
+
             PlayerSettings.SetScriptingBackend(buildTargetGroup, scriptingImplementation);
             CheckIfReimportRequired(cmdParamsMap);
             var buildPlayerOptions = InitializeSpecific(cmdParamsMap, isProduction);
@@ -187,7 +191,7 @@ namespace LionStudios.Editor.AutoBuilder
             return buildPlayerOptions;
         }
 
-        private static string GetDefineSymbolsStr(string[] additionalSymbols, string currentDefineSymbols, 
+        private static string GetDefineSymbolsStr(string[] additionalSymbols, string currentDefineSymbols,
             bool isProduction)
         {
             var currentSymbolsArr = currentDefineSymbols.Split(';');
@@ -217,7 +221,7 @@ namespace LionStudios.Editor.AutoBuilder
             if (bool.TryParse(cmdParamsMap["reimportAssets"], out var isReimport) && isReimport)
             {
                 AssetDatabase.ImportAsset(CommonBuildSettings.ScriptsFolder, ImportAssetOptions.ImportRecursive |
-                                                         ImportAssetOptions.DontDownloadFromCacheServer);
+                                                                             ImportAssetOptions.DontDownloadFromCacheServer);
             }
         }
 
@@ -306,7 +310,7 @@ namespace LionStudios.Editor.AutoBuilder
             // try mac
             string macPath = path.Replace("\\", "/"); // mac finder doesn't like backward slashes
 
-        if (System.IO.Directory.Exists(macPath)) // if path requested is a folder, automatically open insides of that folder
+            if (System.IO.Directory.Exists(macPath)) // if path requested is a folder, automatically open insides of that folder
             {
                 openInsidesOfFolder = true;
             }
@@ -335,7 +339,6 @@ namespace LionStudios.Editor.AutoBuilder
                 e.HelpLink = ""; // do anything with this variable to silence warning about not using it
             }
         }
-#endif 
+#endif
     }
-
 }
